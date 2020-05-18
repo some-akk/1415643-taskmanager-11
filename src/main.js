ad35a82a199ff305bf3cfaabc73d17fcd007d02c
Shvedov Andrey <shvedov.ag@lsrgroup.ru>
2020-05-18 15:03:16 +0300

    Учебный проект: жизнь без интернета

--- src/main.js
+++ src/main.js
@@ -1,14 +1,19 @@
-import API from "./api";
+import API from "./api/index.js";
 import BoardComponent from "./components/board";
 import BoardController from "./controllers/board";
 import FilterController from "./controllers/filter.js";
+import Provider from "./api/provider.js";
 import SiteMenuComponent, {MenuItem} from "./components/site-menu";
 import StatisticsComponent from "./components/statistics";
+import Store from "./api/store.js";
 import TasksModel from "./models/tasks";
 import {render, RenderPosition} from "./utils/render";
 
 const AUTHORIZATION = `Basic a8c01069687cec139f925e5a4dc5429a`;
 const END_POINT = `https://11.ecmascript.pages.academy/task-manager`;
+const STORE_PREFIX = `taskmanager-localstorage`;
+const STORE_VER = `v1`;
+const STORE_NAME = `${STORE_PREFIX}-${STORE_VER}`;
 
 const dateTo = new Date();
 const dateFrom = (() => {
@@ -18,13 +23,15 @@ const dateFrom = (() => {
 })();
 
 const api = new API(END_POINT, AUTHORIZATION);
+const store = new Store(STORE_NAME, window.localStorage);
+const apiWithProvider = new Provider(api, store);
 const tasksModel = new TasksModel();
 const siteMainElement = document.querySelector(`.main`);
 const siteHeaderElement = siteMainElement.querySelector(`.main__control`);
 const siteMenuComponent = new SiteMenuComponent();
 const statisticsComponent = new StatisticsComponent({tasks: tasksModel, dateFrom, dateTo});
 const boardComponent = new BoardComponent();
-const boardController = new BoardController(boardComponent, tasksModel, api);
+const boardController = new BoardController(boardComponent, tasksModel, apiWithProvider);
 const filterController = new FilterController(siteMainElement, tasksModel);
 
 render(siteHeaderElement, siteMenuComponent, RenderPosition.BEFOREEND);
@@ -52,7 +59,26 @@ siteMenuComponent.setOnChange((menuItem) => {
   }
 });
 
-api.getTasks().then((tasks) => {
+apiWithProvider.getTasks().then((tasks) => {
   tasksModel.setTasks(tasks);
   boardController.render();
+});
+
+window.addEventListener(`load`, () => {
+  navigator.serviceWorker.register(`/sw.js`)
+    .then(() => {
+      // успешная регистрация ServiceWorker
+    }).catch(() => {
+      // ошибки при регистрации ServiceWorker
+    });
+});
+
+window.addEventListener(`online`, () => {
+  document.title = document.title.replace(` [offline]`, ``);
+
+  apiWithProvider.sync();
+});
+
+window.addEventListener(`offline`, () => {
+  document.title += ` [offline]`;
 });
